4beae1c1397812f62567679682d828607f52636a
yassine-hamadou <yassinehamadou1@gmail.com>
2021-11-19 01:25:34 +0000

    transferlogic

---
+++ php/transferlogic.php
@@ -0,0 +1,12 @@
+<?php
+require_once ('../includes/db.php');
+require_once ('../includes/functions.php');
+
+if (isset($_POST["send"])) {
+    
+    
+
+}
+else{
+    header("Location: ../php/transfer.php");
+}
